b2224a685a5941e64f35407c5db91922d3a7f863
Dilan Rojas <rojasvargasdilan010@gmail.com>
2025-11-01 23:39:07 -0600

    fonts

---
+++ .config/nvim/lua/plugins/nordic.lua
@@ -0,0 +1,26 @@
+return {
+    'AlexvZyl/nordic.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+        require('nordic').setup({
+        -- This callback can be used to override the colors used in the base palette.
+        on_palette = function(palette) end,
+        -- This callback can be used to override the colors used in the extended palette.
+        after_palette = function(palette) end,
+        -- This callback can be used to override highlights before they are applied.
+        on_highlight = function(highlights, palette) end,
+        -- Enable bold keywords.
+        bold_keywords = false,
+        -- Enable italic comments.
+        italic_comments = true,
+        -- Enable editor background transparency.
+        transparent = {
+            -- Enable transparent background.
+            bg = false,
+            -- Enable transparent background for floating windows.
+            float = false,
+        },
+    })
+    end
+}
