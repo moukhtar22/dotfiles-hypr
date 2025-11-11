c04ced645f500b64231e32728fa0249991f94ee0
Dilan Rojas <rojasvargasdilan010@gmail.com>
2025-11-10 19:30:33 -0600

    improvements

--- .config/hypr/themes/catppuccin-mocha/theme.sh
+++ .config/hypr/themes/catppuccin-mocha/theme.sh
@@ -1,2 +1,2 @@
 # Theme folder | Wallpapers (1st, Any other Monitor) | GTK theme | Nvim theme name
-bash $HOME/.config/hypr/scripts/theme_change.sh catppuccin-mocha 056.jpg 057.jpg dark catppuccin-mocha
+bash $HOME/.config/hypr/scripts/theme_change.sh catppuccin-mocha 058.jpg 057.jpg dark catppuccin-mocha
