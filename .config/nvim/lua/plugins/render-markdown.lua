74043a16a1844d9b4c4b89b99d24a48c6f69475d
Dilan Rojas <rojasvargasdilan010@gmail.com>
2025-11-12 11:49:08 -0600

    theme

--- .config/nvim/lua/plugins/render-markdown.lua
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -42,25 +42,5 @@ return {
       enabled = true,
       disable_background = { 'diff' }
     },
-    paragraph = {
-        -- Useful context to have when evaluating values.
-        -- | text | text value of the node |
-
-        -- Turn on / off paragraph rendering.
-        enabled = false,
-        -- Additional modes to render paragraphs.
-        render_modes = false,
-        -- Amount of margin to add to the left of paragraphs.
-        -- If a float < 1 is provided it is treated as a percentage of available window space.
-        -- Output is evaluated depending on the type.
-        -- | function | `value(context)` |
-        -- | number   | `value`          |
-        left_margin = 0,
-        -- Amount of padding to add to the first line of each paragraph.
-        -- Output is evaluated using the same logic as 'left_margin'.
-        indent = 0,
-        -- Minimum width to use for paragraphs.
-        min_width = 0,
-    },
-  },
+  }
 }
