feba8875aa5eeb34c4681676d0f619a674cc2f65
Dilan Rojas <rojasvargasdilan010@gmail.com>
2025-11-13 14:13:40 -0600

    ...

--- .config/hypr/themes/oasis-light/theme.sh
+++ .config/hypr/themes/oasis-light/theme.sh
@@ -1,2 +1,2 @@
 # Theme folder | Wallpapers (1st, Any other Monitor) | GTK theme | Nvim theme name
-bash $HOME/.config/hypr/scripts/theme_change.sh oasis-light 081.jpg 082.jpg light oasis-day
+bash $HOME/.config/hypr/scripts/theme_change.sh oasis-light 086.jpg 082.jpg light oasis-day
